a3d2180a25b726319d10f8906bdc57ffad4dc6b5
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-03-25 23:25:18 +0900

    enable test refresh

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]]
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.19.0"]]}})
