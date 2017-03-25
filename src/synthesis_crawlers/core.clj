d27d1b12de4f28686aeb47b557e37af1b66bea51
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-03-25 23:50:00 +0900

    create diff funcs

--- src/synthesis_crawlers/core.clj
+++ src/synthesis_crawlers/core.clj
@@ -4,6 +4,17 @@
 
 #_(s/def ::knowledge) 
 
+(defn- diff? [a b]
+  (let [a-b-c (diff a b)]
+    (or (first a) (second b))))
+
 (defn diff-knowledge? 
   [knowledge-a knowledge-b]
-  (diff knowledge-a knowledge-b))
+  (let [a-b-common (diff knowledge-a knowledge-b)]
+    (or (first a-b-common) (second a-b-common))))
+
+(defn diff-extractors?
+  [extractor-a extractor-b]
+  (let [a-b-common (diff extractor-a extractor-b)]
+    (or (first a-b-common) (second a-b-common))))
+
--- test/synthesis_crawlers/core_test.clj
+++ test/synthesis_crawlers/core_test.clj
@@ -3,6 +3,15 @@
             [synthesis-crawlers.core :refer :all]))
 
 
-(deftest knowledge-test
-  (testing "find difference between two knowlege"
-    (is (diff-knowledge? {:title #{"wagahai-ha-neko-de-aru"}} {:title #{:title {}}}))))
+(deftest knowledge-difference-test
+  (testing "returns some if there is no difference between two knowlege otherwise returns nil"
+    (is (diff-knowledge? {:title #{"wagahai-ha-neko-de-aru"}} {:title #{""}}))
+    (is (not (diff-knowledge? {:title #{"wagahai-ha-neko-de-aru"}} {:title #{"wagahai-ha-neko-de-aru"}})))))
+
+(deftest extractors-difference-test
+  (testing "return some if there is no difference between two extractor sets otherwise returns nil"
+    (is (not (diff-extractors? {"site-url" ["container-expr" {:attr "partial-expr"}]}
+                               {"site-url" ["container-expr" {:attr "partial-expr"}]})) "extractors-difference-test-1")
+    (is (diff-extractors? {"site-url" ["container-expr" {:attr "partial-expr"}]
+                           "site-url2" ["container-expr2" {:attr "partial-expr"}]}
+                          {"site-url" ["container-expr" {:attr "partial-expr"}]}) "extractors-difference-test-2")))
